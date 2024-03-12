5b0bb9a54e20b3dd8861e121345e663a90ca7f66
Minh Tran <minh.tran@threatfabric.com>
2024-03-12 15:57:31 +0100

    feat(ignore): skip lazy-lock file

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -51,6 +51,20 @@ return {
     },
     ---@class PluginLspOpts
     opts = {
+      -- ---@type lspconfig.options
+      -- servers = {
+      --   -- pyright will be automatically installed with mason and loaded with lspconfig
+      --   -- pyright = {},
+      --   bashls = {},
+      --   -- gopls = {},
+      --   bicep = {},
+      --   yamlls = {},
+      --   marksman = {},
+      --   powershell_es = {},
+      --   -- azure_pipelines_ls = {},
+      --   terraformls = {},
+      --   helm_ls = {},
+      -- },
       setup = {
         ["*"] = function(servers, opts) end,
       },
