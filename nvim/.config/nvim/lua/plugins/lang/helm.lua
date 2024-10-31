0f128b21d3bbf603f91010196804ad451f783afb
minhtt159 <4847344+minhtt159@users.noreply.github.com>
2024-10-31 12:32:36 +0100

    backup

--- nvim/.config/nvim/lua/plugins/lang/helm.lua
+++ nvim/.config/nvim/lua/plugins/lang/helm.lua
@@ -1,7 +1,10 @@
 return {
   -- NOTE: most of the plugins are installed with
   -- lazyvim.plugins.extras.lang.helm
-  { "towolf/vim-helm", ft = "helm" },
+  {
+    "towolf/vim-helm",
+    ft = "helm",
+  },
   {
     "williamboman/mason.nvim",
     opts = {
