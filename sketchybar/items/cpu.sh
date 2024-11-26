87fd5db6dbcbc79fb7bb6ccdda37d53484e5e32e
Minh Tran <4847344+minhtt159@users.noreply.github.com>
2024-11-26 13:29:06 +0100

    feat: sketchybar reconfigure

--- sketchybar/items/cpu.sh
+++ sketchybar/items/cpu.sh
@@ -10,31 +10,22 @@ sketchybar  --add item cpu.top right \
             \
             --add graph cpu.sys right 55 \
             --set cpu.sys width=0 \
-                  graph.color=$RED \
-                  graph.fill_color=$RED \
-                  label.drawing=off \
-                  icon.drawing=off \
-                  background.padding_right=10 \
-                  background.padding_left=10 \
-                  background.height=30 \
-                  background.drawing=on \
-                  background.color=$TRANSPARENT \
+                          graph.color=$RED \
+                          graph.fill_color=$RED \
+                          label.drawing=off \
+                          icon.drawing=off \
+                          background.padding_right=10 \
+                          background.padding_left=10 \
+                          background.height=30 \
+                          background.drawing=on \
+                          background.color=$TRANSPARENT \
             \
-	--add graph cpu.user right 55 \
-	--set cpu.user graph.color=$BLUE \
-	label.drawing=off \
-	icon.drawing=off \
-	background.padding_right=10 \
-	background.padding_left=10 \
-	background.height=30 \
-	background.drawing=on \
-	background.color=$TRANSPARENT# --add item cpu.percent right \
-	\
-	# label=CPU \
-	\
-	# update_freq=2 \
-	\
-	# background.padding_right=20 # \
-# --set cpu.percent label.font="$FONT:Heavy:12" \
-# icon.drawing=off \
-# mach_helper="$HELPER" \
+            --add graph cpu.user right 55 \
+            --set cpu.user graph.color=$BLUE \
+                          label.drawing=off \
+                          icon.drawing=off \
+                          background.padding_right=10 \
+                          background.padding_left=10 \
+                          background.height=30 \
+                          background.drawing=on \
+                          background.color=$TRANSPARENT
--- sketchybar/items/spaces.sh
+++ sketchybar/items/spaces.sh
@@ -1,31 +1,7 @@
 #!/usr/bin/env sh
 
-##### Adding Mission Control Space Indicators #####
-# Let's add some mission control spaces:
-# https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
-# to indicate active and available mission control spaces.
-
-# SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
-# for i in "${!SPACE_ICONS[@]}"
-# do
-#   sid="$(($i+1))"
-#   space=(
-#     space="$sid"
-#     icon="${SPACE_ICONS[i]}"
-#     icon.padding_left=7
-#     icon.padding_right=7
-#     background.color=0x40ffffff
-#     background.corner_radius=5
-#     background.height=25
-#     label.drawing=off
-#     script="$PLUGIN_DIR/space.sh"
-#     click_script="yabai -m space --focus $sid"
-#   )
-#   sketchybar --add space space."$sid" left --set space."$sid" "${space[@]}"
-# done
-
 sketchybar --add event aerospace_workspace_change
-RED=0xffed8796
+
 for sid in $(aerospace list-workspaces --all); do
     sketchybar --add item "space.$sid" left \
         --subscribe "space.$sid" aerospace_workspace_change \
@@ -34,8 +10,8 @@ for sid in $(aerospace list-workspaces --all); do
               icon.padding_left=10 \
               icon.padding_right=10 \
               icon.highlight_color=$RED \
-              background.color=0x44ffffff \
-              background.corner_radius=20 \
+              background.color=$BLACK \
+              background.corner_radius=15 \
               background.height=30 \
               background.drawing=off \
               click_script="aerospace workspace $sid" \
