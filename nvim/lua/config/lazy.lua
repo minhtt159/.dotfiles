6737a3874859d58417162589c1f580aa3f5e41c8
minhtt159 <4847344+minhtt159@users.noreply.github.com>
2025-04-27 10:10:55 +0200

    chores: optimize configs

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -40,7 +40,6 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ai.copilot" },
     -- { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     -- Lang
-    { import = "lazyvim.plugins.extras.lang.ansible" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.helm" },
@@ -54,8 +53,10 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- Extra mini
-    { import = "lazyvim.plugins.extras.editor.mini-files" },
+    --  GZA$"
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    --  Show hexcode color
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- import/override with your plugins
     { import = "plugins" },
     -- Custom language plugins
