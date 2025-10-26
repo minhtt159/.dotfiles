9b02e961e9765c6e66b44b629e06663647c3117d
minhtt159 <4847344+minhtt159@users.noreply.github.com>
2025-10-26 19:11:01 +0100

    chores: move files

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -17,4 +17,13 @@ return {
       },
     },
   },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        hidden = true, -- for hidden files
+        ignored = true, -- for .gitignore files
+      },
+    },
+  },
 }
